fc109a5328550d4b605cf1a791ed03f1f25ecedd
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-11-14 11:53:39 +0000

    Add Welcome left hand side

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,14 +17,14 @@
         <div class="flex h-full flex-col">
             <div class="flex min-h-0 flex-1 overflow-hidden">
                 <!-- Sidebar-->
-                <nav aria-label="Sidebar" class="hidden lg:block lg:flex-shrink-0 lg:overflow-y-auto lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500">
-                    <div class="relative flex w-24 flex-col space-y-3 p-3 justify-items-center space-y-8">
-                        <div class="text-white flex-shrink-0 grid space-y-4 items-center justify-center">
+                <nav aria-label="Sidebar" class="hidden lg:pt-8 lg:block lg:flex-shrink-0 lg:overflow-y-auto lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500">
+                    <div class="w-24 flex min-h-0 flex-1 flex-col">
+                        <div class="text-white flex flex-1 flex-col overflow-y-auto">
                             <img class="w-12 mx-auto" src="images/MotiveMobLogo.svg" />
                             <img class="w-20" src="images/Divide.svg" />
                         </div>
                         
-                        <div class="grid space-y-8">
+                        <div class="grid flex-grow space-y-8">
                             <a href="#" class="text-white flex-shrink-0 inline-flex items-center justify-center rounded-lg">
                                 <span class="sr-only">Menu</span>
                                 <img class="w-6" src="images/menu.svg" />
@@ -66,25 +66,27 @@
                             </a>
                         </div>
 
-                        <div>
-                            <a href="#" class="text-white flex-shrink-0 inline-flex items-center justify-center rounded-lg">
+                        <div class="flex flex-shrink-0">
+                            <a href="#" class="text-white inline-flex items-center justify-center rounded-lg">
                                 <span class="sr-only">User</span>
                                 <img class="w-20" src="images/User.svg" />
                             </a>
                         </div>
-                        
                     </div>
                 </nav>
 
                 <!-- Main area -->
-                <main class="min-w-0 flex-1 border-t border-gray-200 xl:flex">
-                    <aside class="">
-                        <div class="relative flex h-full w-96 flex-col border-r border-gray-200 bg-gray-100">
-                            <div>Motivate</div>
+                <main class="min-w-0 flex-1 xl:flex">
+                    <aside class="bg-cyan-300 w-2/5">
+                        <div class="relative flex h-full flex-col">
+                            <img src="images/topbg.png" alt="" class="absolute top-0 left-0">
+                            <div class="text-white absolute top-48 left-4 text-5xl z-20"><strong>Motivate</strong><br /> to <strong>Be Your <br />Best</strong></div>
+                            <img src="images/womanblur.png" alt="" class="absolute bottom-0 right-0 w-[420px]">
+                            
                         </div>
                     </aside>
 
-                    <section aria-labelledby="message-heading" class="flex h-full min-w-0 flex-1 flex-col overflow-hidden">
+                    <section aria-labelledby="message-heading" class="flex h-full min-w-0 flex-1 flex-col overflow-hidden bg-purple-500">
                         Motive Mob
                     </section>
                 </main>

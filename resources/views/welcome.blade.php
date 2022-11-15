9566268813209383585b819d32f2a4b36f90319d
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-11-15 15:52:29 +0000

    Update Guest layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,119 @@
 <x-guest-layout>
-    
+    <div id="step-2" class="hidden grid overflow-hidden grid-cols-2 grid-rows-3 gap-0 h-screen">
+        <div class="bg-red-100 col-start-1 col-span-2">
+                Motive Mob
+        </div>
+        <div class="bg-pink-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman4.png" alt="">
+        </div>
+        <div class="bg-yellow-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman1.png" alt="">
+        </div>
+        <div class="bg-pink-400 relative py-4 px-8 text-sm"> 
+            <div>
+                <p class="text-justify">Let your team, friends, and family support your sports success. Have them join your "Mob" to provide you with inspiration through song and word! Use this free app to get motivation from people that care about you. Here's how it works:</p>
+            </div>
+            <div class="absolute bottom-4 left-8">Skip</div>
+        </div>
+        <div class="bg-yellow-400 relative py-4 px-8 text-sm">
+            <div>
+                <p class="text-justify">Let your team, friends, and family support your sports success. Have them join your "Mob" to provide you with inspiration through song and word! Use this free app to get motivation from people that care about you. Here's how it works:</p>
+            </div>
+            <div class="absolute bottom-4 right-20">Back</div>
+            <div class="absolute bottom-4 right-8">Next</div>
+        </div>
+    </div>
+    <div id="step-2" class="hidden grid overflow-hidden grid-cols-2 grid-rows-3 gap-0 h-screen">
+        <div class="bg-red-100 col-start-1 col-span-2">
+                Motive Mob
+        </div>
+        <div class="bg-yellow-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman1.png" alt="">
+        </div>
+        <div class="bg-green-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman2.png" alt="">
+        </div>
+        <div class="bg-yellow-400 relative py-4 px-8 text-sm"> 
+            <div>
+                <p class="text-justify">Let your team, friends, and family support your sports success. Have them join your "Mob" to provide you with inspiration through song and word! Use this free app to get motivation from people that care about you. Here's how it works:</p>
+            </div>
+            <div class="absolute bottom-4 left-8">Skip</div>
+        </div>
+        <div class="bg-green-400 relative py-4 px-8 text-sm">
+            <div>
+                <p class="text-justify">Select your types of motivation - music-based and inspiring quotes. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet odio vitae sapien fringilla consectetur vel ac tortor. </p>
+            </div>
+            <div class="absolute bottom-4 right-20">Back</div>
+            <div class="absolute bottom-4 right-8">Next</div>
+        </div>
+    </div>
+    <div id="step-3" class="hidden grid overflow-hidden grid-cols-2 grid-rows-3 gap-0 h-screen">
+        <div class="bg-red-100 col-start-1 col-span-2">
+                Motive Mob
+        </div>
+        <div class="bg-green-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman2.png" alt="">
+        </div>
+        <div class="bg-blue-300 relative flex justify-center">
+            <img class="absolute bottom-0 w-80" src="images/woman3.png" alt="">
+        </div>
+        <div class="bg-green-400 py-4 px-8 text-sm"> 
+            <div>
+                <p class="text-justify">Select your types of motivation - music-based and inspiring quotes. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet odio vitae sapien fringilla consectetur vel ac tortor. </p>
+            </div>
+            <div class="absolute bottom-4 left-8">Skip</div>
+        </div>
+        <div class="bg-blue-400 py-4 px-8 text-sm">
+            <div>
+                <p class="text-justify">Receive weekly text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet odio vitae sapien fringilla consectetur vel ac tortor. Nulla lacus libero, varius et eros sed, convallis viverra enim. </p>
+            </div>
+            <div class="absolute bottom-4 right-8">Next</div>
+        </div>
+    </div>
+    <div id="step-4" class=" grid overflow-hidden grid-cols-1 grid-rows-4 gap-4 h-screen bg-purple-500">
+        <div class="col-start-1 col-span-1" style="background: linear-gradient(146.67deg, #DC735C 1.12%, #A941D9 122.75%), #D9D9D9;">
+            <img class="w-12 h-12 mx-auto mt-8" src="images/mm-logo1.png" alt="">
+        </div>
+        <div class="bg-purple-600 row-span-2">
+            <div class="flex flex-col items-center -mt-12">
+                <img class="w-24 h-24" src="images/adamdriver.png" alt="">
+                <p>Adam Driver</p>
+                <p>(Teammate) shared with you:</p>
+            </div>
+            <div class="bg-purple-700 text-center py-4 my-4">
+                <p class="text-4xl bold">“You have to fight to reach your dream”</p>
+                <p>- Lionel Messi - </p>
+            </div>                                
+            <div class="bg-purple-600 grid grid-cols-2">
+                <div class="flex justify-center">
+                    <img class="w-44 h-44 -mt-10" src="images/stronger.png" alt="">
+                </div>
+                <div>
+                    <p class="text-md font-extralight">And they thought you might enjoy listening to: </p>
+                    <p class="text-lg">Stronger (Kanye West)</p>
+                    <div class="flex space-x-4 mt-4">
+                        <img class="w-12 h-12" src="images/add.png" alt="">
+                        <img class="w-12 h-12" src="images/play.png" alt="">
+                        <img class="w-12 h-12" src="images/spotify.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-purple-800 py-4 px-8 relative text-sm"> 
+            <div class="grid grid-cols-2 gap-8">
+                <div>
+                    <p class="text-justify">View your <strong>private inspiration page</strong> like this one. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <x-button isFilled>Register Free Account</x-button>
+                    <p class="text-xs text-center mt-2">Your account is free (forever). You can upgrade for $20/year for additional premium features.
+                </div>
+            </div>
+            
+            <div class="absolute bottom-4 left-8">Skip</div>
+            <div class="absolute bottom-4 right-8">
+                Next
+            </div>
+        </div>
+    </div>
 </x-guest-layout>

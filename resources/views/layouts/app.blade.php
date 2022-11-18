7a2fcb4510c4ad46fd9179897144d24eff1fdb07
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-11-18 10:59:01 +0000

    Update layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,98 +13,97 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="">
-         <div class="grid overflow-hidden auto-cols-fr grid-rows-6 gap-0 h-screen">
-            <div class="row-start-1 row-end-7 col-start-1 col-end-5 grid overflow-hidden grid-lines auto-cols-fr grid-rows-1 gap-0 h-screen">
-                <!-- Sidebar-->
-                <div class="col-start-1 col-end-3 lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500 hidden lg:pt-8 lg:block lg:flex lg:flex-col lg:h-screen text-white">                 
-                    <div class="flex flex-col space-y-4">
-                        <img class="w-12 mx-auto" src="images/MotiveMobLogo.svg" />
-                        <img class="w-18" src="images/Divide.svg" />
-                    </div>
-                    
-                    <nav class="flex-grow flex flex-col justify-center space-y-4">
-                        <a href="#" class="group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Menu</span>
-                            <img class="w-6" src="images/menu.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Send</span>
-                            <img class="w-6" src="images/Send.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Heart</span>
-                            <img class="w-6" src="images/heart.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Gallery</span>
-                            <img class="w-6" src="images/Gallery.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Note-3</span>
-                            <img class="w-6" src="images/Note-3.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Headphone</span>
-                            <img class="w-6" src="images/Headphone.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Dollar</span>
-                            <img class="w-6" src="images/dollar.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Setting</span>
-                            <img class="w-6" src="images/Setting.svg" />
-                        </a>
-                    </nav>
-
-                    <div class="">
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">User</span>
-                            <img class="w-20" src="images/User.svg" />
-                        </a>
-                    </div>
+    <body class="font-sans antialiased">
+        <div class="grid overflow-hidden gap-0 h-screen" style="grid-template-columns: 80px 500px 1fr;">
+            <!-- Sidebar-->
+            <div class="hidden lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500 hidden lg:pt-8 lg:block lg:flex lg:flex-col lg:h-screen text-white">                 
+                <div class="flex flex-col space-y-4">
+                    <img class="w-12 mx-auto" src="images/MotiveMobLogo.svg" />
+                    <img class="w-18" src="images/Divide.svg" />
                 </div>
-
-                <!-- Into -->
-                <div class="col-start-3 col-end-12" style="background: linear-gradient(146.67deg, #DC735C 1.12%, #A941D9 122.75%), #83DBEE;">
-                    
-                </div>
-            </div>
-                 
-            <!-- Main area -->
-            <div class="row-start-1 row-end-1 col-start-5 col-end-12 bg-sky-50 relative flex justify-between items-center">
-                <div>
-                    <img class="w-11 h-11 absolute top-8 left-8" src="images/back-arrow.png" alt="">
-                </div>
-                <div class="text-center">
-                    <p>Account Information:</p>
-                    <p class="font-semibold">Lisa Smith</p>
-                </div>
-                <div class="">
+                
+                <nav class="flex-grow flex flex-col justify-center space-y-4">
                     <a href="#" class="group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Menu</span>
+                        <img class="w-6" src="images/menu.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Send</span>
+                        <img class="w-6" src="images/Send.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Heart</span>
+                        <img class="w-6" src="images/heart.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Gallery</span>
+                        <img class="w-6" src="images/Gallery.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Note-3</span>
+                        <img class="w-6" src="images/Note-3.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Headphone</span>
+                        <img class="w-6" src="images/Headphone.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Dollar</span>
+                        <img class="w-6" src="images/dollar.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Setting</span>
+                        <img class="w-6" src="images/Setting.svg" />
+                    </a>
+                </nav>
+
+                <div class="">
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
                         <span class="sr-only">User</span>
                         <img class="w-20" src="images/User.svg" />
                     </a>
                 </div>
             </div>
 
-            <div class="row-start-2 row-end-4 col-start-5 col-end-12">
-                {{$slot}}
-            </div>
-
-            <div class="row-start-4 row-end-7 col-start-5 col-end-12 bg-sky-300 relative">
-                <img src="images/dashboard.png" class="absolute bottom-0 right-0 max-w-xl" alt="">
-                Motivate to be your best
-            </div>
+            <!-- Intro -->
+            <div class="" style="background: linear-gradient(146.67deg, #DC735C 1.12%, #A941D9 122.75%), #83DBEE;">
                 
+            </div>
+                 
+            <!-- Main area -->
+            <div class="bg-sky-50 grid overflow-hidden grid-cols-1 grid-rows-6 gap-0 h-screen">
+                <div class="row-start-1 row-end-2 relative flex justify-between items-center">
+                    <div>
+                        <img class="w-11 h-11 absolute top-8 left-8" src="images/back-arrow.png" alt="">
+                    </div>
+                    <div class="text-center">
+                        <p>Account Information:</p>
+                        <p class="font-semibold">Lisa Smith</p>
+                    </div>
+                    <div class="">
+                        <a href="#" class="group flex items-center justify-center rounded-lg">
+                            <span class="sr-only">User</span>
+                            <img class="w-20" src="images/User.svg" />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row-start-2 row-end-4">
+                    {{$slot}}
+                </div>
+
+                <div class="row-start-4 row-end-7 bg-sky-300 relative">
+                    <img src="images/dashboard.png" class="absolute bottom-0 right-0 max-w-xl" alt="">
+                    Motivate to be your best
+                </div>
+            </div>
         </div>
 
         @stack('modals')
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,80 +13,79 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased h-full overflow-hidden">
-        <div class="flex h-full flex-col">
-            <div class="flex min-h-0 flex-1 overflow-hidden">
-                <!-- Sidebar-->
-                <div aria-label="Sidebar" class="hidden lg:pt-8 lg:block lg:flex lg:flex-col lg:h-screen lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500 text-white">                 
-                    <div class="flex flex-col space-y-4">
-                        <img class="w-12 mx-auto" src="images/MotiveMobLogo.svg" />
-                        <img class="w-18" src="images/Divide.svg" />
-                    </div>
-                    
-                    <nav class="flex-grow flex flex-col justify-center space-y-4">
-                        <a href="#" class="group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Menu</span>
-                            <img class="w-6" src="images/menu.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Send</span>
-                            <img class="w-6" src="images/Send.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Heart</span>
-                            <img class="w-6" src="images/heart.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Gallery</span>
-                            <img class="w-6" src="images/Gallery.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Note-3</span>
-                            <img class="w-6" src="images/Note-3.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Headphone</span>
-                            <img class="w-6" src="images/Headphone.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Dollar</span>
-                            <img class="w-6" src="images/dollar.svg" />
-                        </a>
-
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">Setting</span>
-                            <img class="w-6" src="images/Setting.svg" />
-                        </a>
-                    </nav>
-
-                    <div class="">
-                        <a href="#" class=" group flex items-center justify-center rounded-lg">
-                            <span class="sr-only">User</span>
-                            <img class="w-20" src="images/User.svg" />
-                        </a>
-                    </div>
+    <body class="font-sans antialiased">
+       <div class="grid overflow-hidden gap-0 h-screen" style="grid-template-columns: 80px 460px 1fr;">
+            <!-- Sidebar-->
+            <div class="lg:bg-gradient-to-r lg:from-purple-500 lg:to-violet-500 hidden lg:pt-8 lg:block lg:flex lg:flex-col lg:h-screen text-white">                 
+                <div class="flex flex-col space-y-4">
+                    <img class="w-12 mx-auto" src="images/MotiveMobLogo.svg" />
+                    <img class="w-18" src="images/Divide.svg" />
                 </div>
+                
+                <nav class="flex-grow flex flex-col justify-center space-y-4">
+                    <a href="#" class="group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Menu</span>
+                        <img class="w-6" src="images/menu.svg" />
+                    </a>
 
-                <!-- Main area -->
-                <main class="min-w-0 flex-1 xl:flex text-white">
-                    <aside class="bg-sky-300 w-2/5">
-                        <div class="relative flex h-full flex-col">
-                            <img src="images/topbg.png" alt="" class="absolute top-0 left-0">
-                            <div class=" absolute top-48 left-4 text-5xl z-20"><span class="font-semibold">Motivate</span><br /> to <span class="font-semibold">Be Your <br />Best</span></div>
-                            <img src="images/womanblur.png" alt="" class="absolute bottom-0 right-0 w-[420px]"> 
-                        </div>
-                    </aside>
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Send</span>
+                        <img class="w-6" src="images/Send.svg" />
+                    </a>
 
-                    <section aria-labelledby="message-heading" class="h-full min-w-0 flex-1 overflow-hidden">
-                        {{$slot}}
-                    </section>
-                </main>
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Heart</span>
+                        <img class="w-6" src="images/heart.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Gallery</span>
+                        <img class="w-6" src="images/Gallery.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Note-3</span>
+                        <img class="w-6" src="images/Note-3.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Headphone</span>
+                        <img class="w-6" src="images/Headphone.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Dollar</span>
+                        <img class="w-6" src="images/dollar.svg" />
+                    </a>
+
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">Setting</span>
+                        <img class="w-6" src="images/Setting.svg" />
+                    </a>
+                </nav>
+
+                <div class="">
+                    <a href="#" class=" group flex items-center justify-center rounded-lg">
+                        <span class="sr-only">User</span>
+                        <img class="w-20" src="images/User.svg" />
+                    </a>
+                </div>
+            </div>
+
+            <!-- Intro -->
+            <div class="bg-sky-300">
+                <div class="relative flex h-full flex-col">
+                    <img src="images/topbg.png" alt="" class="absolute top-0 left-0">
+                    <div class=" absolute top-48 left-8 text-5xl z-20 text-white"><span class="font-semibold">Motivate</span><br /> to <span class="font-semibold">Be Your <br />Best</span></div>
+                    <img src="images/womanblur.png" alt="" class="absolute bottom-0 right-0 w-[360px]"> 
+                </div>
+            </div>
+
+            <!-- Main area -->
+            <div class="">
+                <div class="text-white">
+                    {{$slot}}
+                </div>
             </div>
         </div>
     </body>

5bf579a4fe515e1459bdc034100cf8a4b05442cf
tanyafeest <blockchain.engineer215@gmail.com>
2022-11-21 08:22:59 -0600

    fix: change the share link

--- routes/public.php
+++ routes/public.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("{share_link}/{full_name}", [App\Http\Controllers\PublicController::class, "setupSession"])->name("public.inspiration.setupsession");
+Route::get("/share/{share_link}/{full_name}", [App\Http\Controllers\PublicController::class, "setupSession"])->name("public.inspiration.setupsession");

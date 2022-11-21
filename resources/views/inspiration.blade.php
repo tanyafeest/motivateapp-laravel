b40fdd58156f26b48378ac0f4aa39bd7b23e7689
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-11-21 14:16:12 +0000

    Update Inspiration

--- resources/views/inspiration.blade.php
+++ resources/views/inspiration.blade.php
@@ -1,34 +1,68 @@
 <x-app-layout>
-
-        <div class="row-start-2 row-end-4 bg-sky-50 px-8 grid grid-cols-2">
-            <div class="">
-                <h1 class="text-6xl font-bold">Stronger</h1>
-                <p class="text-4xl font-light">Kanye West</p>
-            </div>
-            <div class="">
-                <img src="images/favorite-filled.png" alt="">
-                <img src="images/shuffle.png" alt="">
-                <img src="images/repeat.png" alt="">
-                <img src="images/share.png" alt="">
-            </div>
+    <div class="row-start-2 row-end-4 bg-sky-50 px-8 grid grid-cols-2 flex items-end pb-8">
+        <div class="">
+            <h1 class="text-6xl font-bold">Stronger</h1>
+            <p class="text-4xl font-light">Kanye West</p>
         </div>
-
-        <div class="row-start-4 row-end-9 px-8">
-            <table class="table w-full">
-                <tr>
-                    <td><img class="h-8 w-8" src="images/played.png" alt=""></td>
-                    <td><span class="font-bold text-xl">Stronger</span><br /><span class="font-light text-sm">Kanye West</span></td>
-                    <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
-                    <td>4:07</td>
-                </tr>
-                <tr>
-                    <td><img class="h-8 w-8" src="images/play-list.png" alt=""></td>
-                    <td><span class="font-bold text-xl">Stronger</span><br /><span class="font-light text-sm">Kanye West</span></td>
-                    <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
-                    <td>4:07</td>
-                </tr>
-            </table>
+        <div class="flex space-x-8 justify-end">
+            <img class="w-4 h-4" src="images/favorite-filled.png" alt="">
+            <img class="w-4 h-4" src="images/shuffle.png" alt="">
+            <img class="w-4 h-4" src="images/repeat.png" alt="">
+            <img class="w-4 h-4" src="images/share.png" alt="">
         </div>
     </div>
-    
+
+    <div class="row-start-4 row-end-9 px-8">
+        <table class="my-4 table w-full">
+            <tr>
+                <td><img class="h-8 w-8" src="images/played.png" alt=""></td>
+                <td><span class="font-bold text-xl">Stronger</span><br /><span class="font-light text-sm">Kanye West</span></td>
+                <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
+                <td>4:07</td>
+            </tr>
+            <tr>
+                <td><img class="h-8 w-8" src="images/play-list.png" alt=""></td>
+                <td><span class="font-bold text-xl">Rockstar</span><br /><span class="font-light text-sm">Post Malone featuring 21 Savage</span></td>
+                <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
+                <td>4:07</td>
+            </tr>
+            <tr>
+                <td><img class="h-8 w-8" src="images/play-list.png" alt=""></td>
+                <td><span class="font-bold text-xl">Stronger</span><br /><span class="font-light text-sm">Kanye West</span></td>
+                <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
+                <td>4:07</td>
+            </tr>
+            <tr>
+                <td><img class="h-8 w-8" src="images/play-list.png" alt=""></td>
+                <td><span class="font-bold text-xl">Rockstar</span><br /><span class="font-light text-sm">Post Malone featuring 21 Savage</span></td>
+                <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
+                <td>4:07</td>
+            </tr>
+            <tr>
+                <td><img class="h-8 w-8" src="images/play-list.png" alt=""></td>
+                <td><span class="font-bold text-xl">Stronger</span><br /><span class="font-light text-sm">Kanye West</span></td>
+                <td><img class="h-4 w-4" src="images/favorite.png" alt=""></td>
+                <td>4:07</td>
+            </tr>
+        </table>
+
+        <div class="flex flex-col items-center">
+            <div class="w-full">
+                <div class="my-4"><hr /></div>
+                <div class="flex justify-between text-gray-400 text-xs font-medium">
+                    <div>
+                        1:43
+                    </div>
+                    <div>
+                        3:58
+                    </div>
+                </div>
+            </div>
+            <div class="flex space-x-4 items-center">
+                <div><img class="w-5 h-5" src="images/previous.png" alt=""></div>
+                <div><img src="images/play_arrow.png" alt=""></div>
+                <div><img class="w-5 h-5" src="images/next.png" alt=""></div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>

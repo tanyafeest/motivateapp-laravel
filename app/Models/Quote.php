0540d3aa3655db9aadb07603cf73632a32ebc790
tanyafeest <blockchain.engineer215@gmail.com>
2022-12-13 14:27:15 -0600

    feat: implement some local scopes in Quote

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -19,4 +19,48 @@ class Quote extends Model
         'quote',
         'author'
     ];
+
+    /**
+     * Scope a query to only include confidence quotes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeConfidence($query)
+    {
+        $query->where('category', 'Confidence');
+    }
+
+    /**
+     * Scope a query to only include potential quotes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopePotential($query)
+    {
+        $query->where('category', 'Potential');
+    }
+
+    /**
+     * Scope a query to only include determination quotes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeDetermination($query)
+    {
+        $query->where('category', 'Determination');
+    }
+
+    /**
+     * Scope a query to only include resilience quotes.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeResilience($query)
+    {
+        $query->where('category', 'Resilience');
+    }
 }

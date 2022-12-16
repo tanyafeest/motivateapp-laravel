151b094e4988a254505c28839093814598800c70
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-12-16 10:09:18 +0000

    Update onboarding.blade.php

--- resources/views/livewire/onboarding.blade.php
+++ resources/views/livewire/onboarding.blade.php
@@ -202,7 +202,7 @@
     </div>
 
     <!-- purple -->
-    <div id="step6" class="flex flex-col {{ $currentMStep != 6 ? 'hidden' : '' }} {{ $currentStep != 6 ? 'lg:hidden' : 'lg:block' }} items-center h-screen relative" style="background: linear-gradient(146.67deg, #7A5CDC 1.12%, #AF55D8 122.75%);">
+    <div id="step6" class="flex flex-col {{ $currentMStep != 6 ? 'hidden' : '' }} {{ $currentStep != 6 ? 'lg:hidden' : 'lg:block' }} items-center h-screen relative lg:grid lg:grid-rows-3" style="background: linear-gradient(146.67deg, #7A5CDC 1.12%, #AF55D8 122.75%); ">
         <img class="w-[48px] h-[46px] absolute top-4 left-4" src="images/back.svg" alt="">
         <img class="w-[48px] h-[46px] mt-10 mb-6 lg:hidden" src="images/mm-logo1.png" alt="">
             
@@ -211,30 +211,33 @@
             <p class="text-xl font-semibold">Adam Driver</p>
             <p class="text-xs"><span class="font-light">(Teammate)</span> shared with you:</p>
         </div>
-
-         <div class="flex flex-col w-full p-4 mb-6" style="background: rgb(120,84,202); background: linear-gradient(95deg, rgba(120,84,202,1) 0%, rgba(142,81,200,1) 100%);">
-            <div class="px-4 mx-auto lg:max-w-3xl">
-                <p class="text-2xl font-semibold text-center">“You have to fight to reach your dream”</p>
-                <p class="text-right">- Lionel Messi - </p>
-            </div>
-        </div> 
-            
-        <div class="flex px-4 mb-8 -mt-12 lg:mb-2">       
-            <div class="flex justify-center">
-                <img class="w-[200px]" src="images/stronger.png" alt="">
-            </div>
-            <div class="mt-10 ml-4">
-                <p class="text-md font-extralight">And they thought you might enjoy listening to: </p>
-                <p class="text-lg">Stronger (Kanye West)</p>
-                <div class="flex mt-4 space-x-4">
-                    <img class="w-12 h-12" src="images/add.png" alt="">
-                    <img class="w-12 h-12" src="images/play.png" alt="">
-                    <img class="w-12 h-12" src="images/spotify1.png" alt="">
+        
+        <div>
+            <div class="flex flex-col w-full p-4 mb-6" style="background: rgb(120,84,202); background: linear-gradient(95deg, rgba(120,84,202,1) 0%, rgba(142,81,200,1) 100%);">
+                <div class="px-4 mx-auto lg:max-w-3xl">
+                    <p class="text-2xl font-semibold text-center">“You have to fight to reach your dream”</p>
+                    <p class="text-right">- Lionel Messi - </p>
+                </div>
+            </div> 
+                
+            <div class="flex justify-center px-4 mb-8 -mt-12 lg:mb-2">       
+                <div class="flex justify-center">
+                    <img class="w-[200px]" src="images/stronger.png" alt="">
+                </div>
+                <div class="mt-10 ml-4">
+                    <p class="text-md font-extralight">And they thought you might enjoy listening to: </p>
+                    <p class="text-lg">Stronger (Kanye West)</p>
+                    <div class="flex mt-4 space-x-4">
+                        <img class="w-12 h-12" src="images/add.png" alt="">
+                        <img class="w-12 h-12" src="images/play.png" alt="">
+                        <img class="w-12 h-12" src="images/spotify1.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="flex flex-col items-center w-full h-full px-8 pt-4 mt-8 lg:flex-row lg:space-x-8" style="background: #7d52d9;">
+         
+        <div class="flex flex-col items-center w-full h-full px-8 pt-4 mt-8 lg:flex-row lg:h-fit" style="background: #7d52d9;">
             <p class="mb-8 text-sm text-center text-justify">View your <span class="font-bold">private inspiration page</span> like this one. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <div class="flex flex-col items-center">
                 <a class="px-8 py-2 font-medium text-blue-500 transition duration-150 ease-in-out bg-white rounded-3xl" href="/registration">Register Free Account</a>

f6991de12670e6df0d01d8e31ed51abb54315df4
tanyafeest <blockchain.engineer215@gmail.com>
2023-01-03 14:37:43 -0600

    feat: twilio, ipbase

--- app/Actions/Util/IpBase.php
+++ app/Actions/Util/IpBase.php
@@ -5,18 +5,24 @@ namespace App\Actions\Util;
 use GuzzleHttp\Client;
 
 class IpBase {
-    /*
-    *   Calculate grade year
-    *
-    *   @param  int current_grade
-    *   @return int grade_year
+    public $client = null;
+
+    public function __construct()
+    {
+        $this->client = new Client(['base_uri' => 'https://api.ipbase.com']);
+    }
+
+    /**
+     * Get ip information
+     *
+     * @param  String   $ip
+     * @return Object   $info
     */
     public function info($ip)
     {
         try {
-            $client = new Client(['base_uri' => 'https://api.ipbase.com']);
 
-            $response = $client->get('/v2/info?apikey=' . config('ipbase.api_key') . '&ip=' . $ip);
+            $response = $this->client->get('/v2/info?apikey=' . config('ipbase.api_key') . '&ip=' . $ip);
 
             if($stream = $response->getBody()) {
                 $size = $stream->getSize();

8e46feecb50c9f143bfe30eadf5b00791b80e55e
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2023-01-09 12:03:31 +0000

    Update playlist mobile layout

--- resources/views/inspiration-playlist.blade.php
+++ resources/views/inspiration-playlist.blade.php
@@ -4,10 +4,10 @@
             @livewire('header-profile')
         </div>
         
-        <div class="">
+        <div class="hidden lg:block">
             <div class="flex grid items-end grid-cols-2 row-span-2 px-8 pb-8 bg-sky-50">
-                <div class="">
-                    <h1 class="text-6xl font-bold">Stronger</h1>
+                <div class="mt-24">
+                    <h1 class="text-6xl font-semibold">Stronger</h1>
                     <p class="text-4xl font-light">Kanye West</p>
                 </div>
                 <div class="flex justify-end space-x-8">
@@ -70,6 +70,32 @@
                         <div><img class="w-5 h-5" src="images/next.png" alt=""></div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="lg:hidden flex flex-col mt-8 px-8">
+            <img class="w-64 mx-auto" src="images/stronger-title.png" alt="">
+
+            <div class="mt-4 text-white items-start">
+                <h1 class="text-4xl font-semibold">Stronger</h1>
+                <p class="text-2xl font-light">Kanye West</p>
+            </div>
+
+            <div>
+                <img class="mx-auto mt-4" src="images/player.png" alt="">
+            </div>
+
+            <div class="flex justify-around mt-12">
+                <img class="" src="images/player-favorite.png" alt="">
+                <img class="" src="images/player-shuffle.png" alt="">
+                <img class="" src="images/player-repeat.png" alt="">
+                <img class="" src="images/player-share.png" alt="">
+            </div>
+
+            <div class="mt-12 space-y-4 text-white">
+                <div class="flex space-x-4 item-center"><img src="images/playlist_add.png" alt=""> <span>Add to Playlist</span></div>
+                <div class="flex space-x-4 item-center"><img src="images/person_search.png" alt=""> <span>View Artist</span></div>
+                <div class="flex space-x-4 item-center"><img src="images/article.png" alt=""> <span>Show Lyrics</span></div>
             </div>
         </div>
     </div>
--- resources/views/livewire/album.blade.php
+++ resources/views/livewire/album.blade.php
@@ -1,8 +1,6 @@
 <div id="intro" class="hidden lg:relative lg:flex lg:flex-col lg:items-center lg:justify-center" style="background-image:url('images/stronger.jpg'); background-position:center;">
     <img class="w-48" src="images/stronger-title.png" alt="">
 
-   
-
     <div class="text-white absolute inset-x-0 bottom-0 p-4" style="background: rgba(39, 37, 111, 0.4);">
         <img class="w-24 absolute -translate-y-16 -translate-x-4" src="images/play-pause.png" alt="">
         <div class="flex justify-between items-center mt-4">
@@ -17,6 +15,5 @@
                 <img src="images/article.png" alt="">
             </div>
         </div>
-        
     </div>
 </div>

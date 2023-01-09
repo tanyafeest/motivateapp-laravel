70c015771a4ce6e3c188a724bed0ef13aa8c628e
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2023-01-09 14:14:07 +0000

    Update inspiration-past.blade.php

--- resources/views/inspiration-past.blade.php
+++ resources/views/inspiration-past.blade.php
@@ -4,178 +4,89 @@
             @livewire('header-profile')
         </div>
         
-        <div class="">
+        <div class="bg-white lg:bg-gray-50 h-screen">
             <div class="px-8 row-span-7">
-                <p class="mt-4 text-2xl font-bold">Past Motivation:</p>
+                <p class="mt-4 text-2xl font-semibold">Past Motivation:</p>
 
-                <div class="grid grid-cols-2 gap-8 mt-8">
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
+                <div class="grid lg:grid-cols-2 gap-8 mt-8 divide-y lg:divide-none">
+                    <div class="lg:p-4 bg-white lg:shadow lg:round-lg">
+                        <div class="grid grid-cols-8 gap-1 items-center">
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/Message3.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500">Quote:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p>"I can't erase what you done to me."</p></div>
+
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/song.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Song:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p><a class="underline text-blue-500" href="#">First Time</a></p></div>
+              
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/send.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Shared by:</p></div>
+
+                            <div class="col-span-1"><img class="w-6 mx-auto" src="images/danielsmith.png" alt=""></div>
+                            <div class="col-span-7"><a class="underline text-blue-500" href="#">Daniel Smith</a></div>
+                        </div>    
                     </div>
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
+
+                    <div class="lg:p-4 bg-white lg:shadow lg:round-lg pt-2">
+                        <div class="grid grid-cols-8 gap-1 items-center">
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/Message3.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500">Quote:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p>"I can't erase what you done to me."</p></div>
+
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/song.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Song:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p><a class="underline text-blue-500" href="#">First Time</a></p></div>
+              
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/send.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Shared by:</p></div>
+
+                            <div class="col-span-1"><img class="w-6 mx-auto" src="images/danielsmith.png" alt=""></div>
+                            <div class="col-span-7"><a class="underline text-blue-500" href="#">Daniel Smith</a></div>
+                        </div>    
                     </div>
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
+
+                    <div class="lg:p-4 bg-white lg:shadow lg:round-lg pt-2">
+                        <div class="grid grid-cols-8 gap-1 items-center">
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/Message3.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500">Quote:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p>"I can't erase what you done to me."</p></div>
+
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/song.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Song:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p><a class="underline text-blue-500" href="#">First Time</a></p></div>
+              
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/send.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Shared by:</p></div>
+
+                            <div class="col-span-1"><img class="w-6 mx-auto" src="images/danielsmith.png" alt=""></div>
+                            <div class="col-span-7"><a class="underline text-blue-500" href="#">Daniel Smith</a></div>
+                        </div>    
                     </div>
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
-                    </div>
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
-                    </div>
-                    <div class="p-4 bg-white shadow round-lg">
-                        <table class="table">
-                            <tr>
-                                <td><img src="images/Message3.png" alt=""></td>
-                                <td><p class="text-red-500">Quote:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p>"I can't erase what you done to me."</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/song.png" alt=""></td>
-                                <td><p class="text-red-500">Song:</p></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><p><a href="#">First Time</a></p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/send.png" alt=""></td>
-                                <td><p class="text-red-500">Shared by::</p></td>
-                            </tr>
-                            <tr>
-                                <td><img src="images/danielsmith.png" alt=""></td>
-                                <td><p><a href="#">Daniel Smith</a></p></td>
-                            </tr>
-                        </table>    
+
+                    <div class="lg:p-4 bg-white lg:shadow lg:round-lg pt-2">
+                        <div class="grid grid-cols-8 gap-1 items-center">
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/Message3.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500">Quote:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p>"I can't erase what you done to me."</p></div>
+
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/song.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Song:</p></div>
+
+                            <div class="col-start-2 col-span-7"><p><a class="underline text-blue-500" href="#">First Time</a></p></div>
+              
+                            <div class="col-span-1"><img class="w-4 mx-auto" src="images/send.png" alt=""></div>
+                            <div class="col-span-7"><p class="text-red-500 col-span-4">Shared by:</p></div>
+
+                            <div class="col-span-1"><img class="w-6 mx-auto" src="images/danielsmith.png" alt=""></div>
+                            <div class="col-span-7"><a class="underline text-blue-500" href="#">Daniel Smith</a></div>
+                        </div>    
                     </div>
                 </div>
             </div>
